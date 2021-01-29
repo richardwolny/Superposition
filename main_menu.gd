ef272f7ee0621e11bea676cf3e18611895478bc0
Asher Glick <asher.glick@gmail.com>
2021-01-29 16:16:19 -0600

    auto set the server if this is the web client

--- main_menu.gd
+++ main_menu.gd
@@ -6,7 +6,9 @@ signal start_game()
 
 func _ready():
 	if not (OS.has_feature("dungeon_master") or OS.has_feature("editor")):
-		$VBoxContainer/VBoxContainer/Host.visible = false
+		$VBoxContainer/VBoxContainer/HostGame.visible = false
+		$VBoxContainer/VBoxContainer/IpAddress.visible = false
+		$VBoxContainer/VBoxContainer/IpAddress.text = "server.demetermine.com"
 
 	$VBoxContainer/Version.text = "Version: " + Version.id()
 
